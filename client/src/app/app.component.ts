eed69505c4931ccb8f7709132782036d97d979c6
polish <polish1987@gmail.com>
2020-08-24 22:30:37 +0200

    feat(components): Create initial monolith component for the app

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,21 +1,78 @@
-import { Component } from '@angular/core';
-import {TodoDataService} from './services/todo-data.service';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import {TaskDataService} from './services/task-data.service';
 import { first } from 'rxjs/operators';
+import { Todo } from './models/todo';
+
+enum ListState {
+  listing,
+  editing,
+  add,
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
 
-  constructor(private taskDataService: TodoDataService) {
 
-    taskDataService.getAllTasks().pipe(first()).subscribe(task => {
-      console.log('task', task);
+  tasks: Todo[] = [];
+  listStates = ListState;
+  descriptionMaxLength = 100;
+
+  descriptionValue = '';
+  titleValue = '';
+
+  descriptionLength = this.descriptionMaxLength;
+
+  listState = this.listStates.listing;
+
+  constructor(private taskDataService: TaskDataService) {
+    this.getTasks();
+  }
+
+  getTasks(): void {
+    this.taskDataService.getAllTasks().pipe(first()).subscribe(tasks => {
+      this.tasks = tasks.sort((a: any, b: any) => a.createdAt > b.createdAt ? 1 : -1);
     });
   }
 
-  title = 'taskmaster-app';
+  clearInputs(): void {
+    this.descriptionValue = '';
+    this.titleValue = '';
+    this.descriptionLength = this.descriptionMaxLength;
+  }
 
+  toggleListState(): void {
+    this.listState = ListState.add;
+  }
+
+  checkLength(): void {
+    this.descriptionLength = this.descriptionMaxLength - this.descriptionValue.length;
+  }
+  submitTask(event): void {
+    console.log('this.titleValue', this.titleValue)
+    const title = this.titleValue;
+    const description = this.descriptionValue;
+    const currentTodo = new Todo({title, description});
+    if (title.length > 0){
+      this.taskDataService.addTask(currentTodo).pipe(first()).subscribe(result => {
+        console.log('result', result);
+        this.getTasks();
+        this.clearInputs();
+      });
+    }
+  }
+
+  deleteTask(id): void {
+    this.taskDataService.deleteTaskById(id).pipe(first()).subscribe(result => {
+      console.log('result', result);
+      this.getTasks();
+
+    });
+  }
 
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
